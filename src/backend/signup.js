d093e3021eeb87c7c3c9e941378e53df9ac83160
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-10-30 17:46:37 -0500

    Basic express routing and form submission works

--- src/backend/signup.js
+++ src/backend/signup.js
@@ -1,5 +1,18 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
+const PORT = 8080;
 
-app.get('/')
+app.use(bodyParser.urlencoded());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(import.meta.dirname, '..', 'frontend', 'signup.html'));
+});
+
+app.post('/signupForm', (req, res) => {
+    res.send(req.body.passwordRepeat);
+});
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
